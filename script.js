3f7b588159bc7123957b71416bd872f169bbd204
npwhl <taleo.sch@gmail.com>
2024-08-08 22:42:43 -0400

    Fix grid calculation error
    The grid now displays properly when the webpage is viewed at 100%.
    The grid displays inaccurately when the webpage is zoomed in and out,
    will have to fix later.

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
 const easel = document.querySelector("#easel");
-const GRID_HEIGHT = 1000;
-
+const GRID_HEIGHT = 800;
+const BORDER_WIDTH = 1;
 easel.style.height = `${GRID_HEIGHT}px`;
 easel.style.width = `${GRID_HEIGHT}px`;
 
 function createGrid(rowSize) {
-    let squareHeight = GRID_HEIGHT/rowSize;
+    let squareHeight = GRID_HEIGHT/rowSize - 2*BORDER_WIDTH;
     for(let count = 0; count < rowSize*rowSize; count++) {
         const square = document.createElement("div");
         square.classList.add("square");
         square.style.height = `${squareHeight}px`;
         square.style.width = `${squareHeight}px`;
+        square.style.border = `${BORDER_WIDTH}px solid black`;
         easel.appendChild(square);
     }
 }
