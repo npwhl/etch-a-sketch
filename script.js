1e0b8ef9d7b5ee63bb1c17970985fd7aa37de246
npwhl <taleo.sch@gmail.com>
2024-08-09 14:47:48 -0400

    Fix border sizing issue and centered easel
    Also changed the calculation of the square height to be a percentage of
    the easel instead of a set pixel to reduce potential inaccuracies in size
    calculations.

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 const easel = document.querySelector("#easel");
 const GRID_HEIGHT = 800;
-const BORDER_WIDTH = 1;
+let BORDER_WIDTH = 1;
 easel.style.height = `${GRID_HEIGHT}px`;
 easel.style.width = `${GRID_HEIGHT}px`;
 
 function createGrid(rowSize) {
-    let squareHeight = GRID_HEIGHT/rowSize - 2*BORDER_WIDTH;
+    let squareDimen = 1/(rowSize)*100;
     for(let count = 0; count < rowSize*rowSize; count++) {
         const square = document.createElement("div");
         square.classList.add("square");
-        square.style.height = `${squareHeight}px`;
-        square.style.width = `${squareHeight}px`;
-        square.style.border = `${BORDER_WIDTH}px solid black`;
+        square.style.height = `${squareDimen}%`;
+        square.style.width = `${squareDimen}%`;
+        square.style.border = `${BORDER_WIDTH}px solid gray`;
         easel.appendChild(square);
     }
 }
